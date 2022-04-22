8f9736a974f207142fcf0fdf2fa369d301dde451
Thomas <schmiddla@gmail.com>
2022-04-22 15:19:14 +0200

    solutions part 2

--- src/components/SolutionInfo.jsx
+++ src/components/SolutionInfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import useBreakpoints from '../custom/useBreakpoint';
 
 export default function SolutionInfo({
   imgLeft = true,
@@ -7,12 +8,25 @@ export default function SolutionInfo({
   text,
   children,
 }) {
-  console.log(imgLeft);
+  const point = useBreakpoints();
+
+  function getLabelBg(left) {
+    if (left) {
+      return 'bg-amber-500';
+    } else {
+      return 'bg-teal-500';
+    }
+  }
+
   function getDivImage() {
     return (
       <div className="info-img w-full md:w-2/3 relative">
         <img className="w-full" src={imgUrl} alt="" />
-        <div className="absolute top-2 text-center w-full font-bold text-xl sm:text-2xl md:text-3xl lg:text-4xl p-0 sm:p-2 md:p-4 bg-teal-500">
+        <div
+          className={`absolute top-2 text-slate-100 text-center w-full font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl p-0 sm:p-2 md:p-4 ${getLabelBg(
+            imgLeft
+          )}`}
+        >
           {title}
         </div>
       </div>
@@ -25,7 +39,7 @@ export default function SolutionInfo({
         {/* <div className="font-bold text-2xl md:text-3xl lg:text-4xl bg-teal-500 p-4 mb-2">
           {title}
         </div> */}
-        <div className="text px-5 pb-3 h-full flex items-center justify-center text-xl sm:text-xl md:text-xl lg:text-2xl">
+        <div className="text px-5 pb-5 md:pb-0 pt-1 md:pt-0 h-full flex items-center justify-center text-xl sm:text-xl md:text-xl lg:text-2xl">
           {children}
         </div>
       </div>
@@ -35,7 +49,7 @@ export default function SolutionInfo({
   return (
     <>
       <div className="flex flex-wrap justify-center border-0">
-        {imgLeft ? (
+        {imgLeft || point === 'sm' || point === 'xs' ? (
           <>
             {getDivImage()} {getDivText()}
           </>
