89af322683cc3f202a3677ff65daf4d0b0708f1b
Olin Kirkland <me@olinkirk.land>
2022-04-22 15:50:44 +0200

    Footer progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,40 @@ export default function Footer() {
   return (
     <footer>
       <div className="footer-primary">
-        <div className="about-us">
-
+        <div className="footer-panel about-us">
+          <h3>About Us</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            sint maxime deleniti! Illum in natus labore, culpa quis ab quos!
+          </p>
+          <div className="social-icons">
+            <a href="#">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
         </div>
-        <div className="get-in-touch">
 
+        <div className="footer-panel get-in-touch">
+          <h3>Get In Touch</h3>
         </div>
-        <div className="community">
+        <div className="footer-panel community">
+          <h3>Community</h3>
           <ul>
-            <li>Volunteer</li>
-            <li>Take a Class</li>
-            <li>Donate</li>
+            <li>
+              <a href="#">😄 Volunteer</a>
+            </li>
+            <li>
+              <a href="#">👩‍🏫 Take a Class</a>
+            </li>
+            <li>
+              <a href="#">💵 Donate</a>
+            </li>
           </ul>
         </div>
       </div>
