63da632f7866c203cb95dc8f7967bb31022c2c37
James Boyle <james.patrick.boyle92@gmail.com>
2022-04-22 11:27:32 +0200

    skeleton start

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import MapPage from './components/MapPage';
-import './style/app.scss';
-import AuthState from './context/AuthContext';
-import Page403 from './components/403';
-import Page404 from './components/404';
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import MapPage from "./components/MapPage";
+import "./style/app.scss";
+import AuthState from "./context/AuthContext";
+/* import Page403 from './components/403';
+import Page404 from './components/404'; */
+
+//CREATE & IMPORT YOUR FUNCTION WHEN READY
 
 function App() {
   return (
     <>
       <AuthState>
-        <Routes>
-          <Route path={`/`} element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="map" element={<MapPage />} />
-          </Route>
-          <Route path={`/403`} element={<Page403 />} />
-          <Route element={<Page404 />} /> {/* ! todo doesnt work  */}
-        </Routes>
+        <Navbar />
+        <Hero />
+        <Map />
+        <Causes />
+        <Solutions />
+        <Substancs />
       </AuthState>
     </>
   );
