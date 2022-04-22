0db296c3a95c8b749b6163ab62ecf06a2730424e
Olin Kirkland <me@olinkirk.land>
2022-04-22 15:36:47 +0200

    Move anchor link styling to app.scss

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,5 +2,24 @@ import React from 'react';
 import './style/footer.scss';
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <div className="footer-primary">
+        <div className="about-us">
+
+        </div>
+        <div className="get-in-touch">
+
+        </div>
+        <div className="community">
+          <ul>
+            <li>Volunteer</li>
+            <li>Take a Class</li>
+            <li>Donate</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-secondary"></div>
+    </footer>
+  );
 }
